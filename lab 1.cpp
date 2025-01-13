f48ba4437a8ca749eeda3a4ed4b295ac379586dd
mijubaz <bajracharyamiju@gmail.com>
2025-01-13 21:30:09 +0545

    Add files via upload

---
+++ lab 1.cpp
@@ -0,0 +1,31 @@
+#include <graphics.h>
+#include <math.h>
+
+void ddaLineDrawing(int x1, int y1, int x2, int y2) {
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+
+    int steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy);
+
+    float xIncrement = dx / (float)steps;
+    float yIncrement = dy / (float)steps;
+
+    float x = x1, y = y1;
+
+    for (int i = 0; i <= steps; i++) {
+        putpixel(round(x), round(y), WHITE);
+        x += xIncrement;
+        y += yIncrement;
+    }
+}
+
+int main() {
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "");
+
+    ddaLineDrawing(100, 100, 300, 300);
+
+    getch(); 
+    closegraph();
+    return 0;
+}
